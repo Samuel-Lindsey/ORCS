75500fbcc1a2a92b674ee44346fab45d6da61980
Samuel Lindsey <samuel.lindsey@icloud.com>
2017-11-05 20:00:45 -0500

    added images and started script for Events dropdown

--- js/script.js
+++ js/script.js
@@ -4,3 +4,14 @@ $('.hamburger').on('click', function(e) {
   // If menu is already showing, slide it up. Otherwise, slide it down.
   $('.menu').toggleClass('slide-down');
 });
+
+
+$('.eventInfo').on('click', function(e) {
+  // Prevent link from jumping to the top of the page
+  e.preventDefault();
+  // If menu is already showing, slide it up. Otherwise, slide it down.
+  $(".details").toggleClass("data-hidden");
+
+});
+//$('eventInfo').attr( "style", "display: table-cell");
+$('.eventInfo').append ('<img src="img/down_arrow.png" alt="down arrow" width=" 20px">');
