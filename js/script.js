63b64ae05e3b05470527927a000a050e9b9cc402
Samuel Lindsey <samuel.lindsey@icloud.com>
2017-11-06 20:00:40 -0500

    Completed the Event details dropdown javascript

--- js/script.js
+++ js/script.js
@@ -8,10 +8,13 @@ $('.hamburger').on('click', function(e) {
 
 $('.eventInfo').on('click', function(e) {
   // Prevent link from jumping to the top of the page
-  e.preventDefault();
-  // If menu is already showing, slide it up. Otherwise, slide it down.
-  $(".details").toggleClass("data-hidden");
 
+  // If menu is already showing, slide it up. Otherwise, slide it down.
+
+  var id = $(this).data("id");
+  var selector = ".details[data-id=" + id + "]"
+
+  $(selector).toggleClass("data-hidden")
 });
-//$('eventInfo').attr( "style", "display: table-cell");
+
 $('.eventInfo').append ('<img src="img/down_arrow.png" alt="down arrow" width=" 20px">');
